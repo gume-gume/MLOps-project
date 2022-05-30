ed271711e0687449917b8ee2c6c91f53b5484010
jang-1996 <altu1996@naver.com>
2022-05-30 01:20:24 +0000

    <chore>
    data 폴더 삭제, minio 삭제, pre-commit 설정

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 from routers import income
 from schemas.response import error_responses
-# exception handler
-from utils.handlers import *
-from fastapi.exceptions import RequestValidationError
+from utils.handlers import (
+    custom_http_exception_handler,
+    custom_validation_exception_handler,
+    custom_app_exception_handler,
+)
+
 from utils.app_exceptions import AppExceptionCase
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-app = FastAPI(responses={**error_responses}) ## --> 에러 500, 404,422,429 Not found 정의
+app = FastAPI(responses={**error_responses})
 
 app.include_router(router=income.router)
 
-@app.get('/')
+
+@app.get("/")
 def root():
-    return 'Welcome to gume-gume'
+    return "Welcome to gume-gume"
+
 
 app.add_exception_handler(StarletteHTTPException, custom_http_exception_handler)
 app.add_exception_handler(RequestValidationError, custom_validation_exception_handler)
--- routers/income.py
+++ routers/income.py
@@ -1,7 +1,4 @@
-import imp
-from fastapi import APIRouter,Depends
-from fastapi.responses import JSONResponse
-
+from fastapi import APIRouter, Depends
 from db.database import SessionLocal, get_db
 
 from schemas.request import IncomeBody
@@ -9,26 +6,38 @@ from schemas.response import Item
 
 import redisai as rai
 
-from service.app_service import *
+from service.app_service import TrainService, PredictService
 from utils.service_result import handle_result
 from config import settings
 
 router = APIRouter()
 
 db = SessionLocal()
+
 client = None
-@router.on_event('startup')
+
+
+@router.on_event("startup")
 def start_up():
     global client
-    client = rai.Client(host='localhost', port=6379, health_check_interval=30)
+    client = rai.Client(
+        host=settings.REDIS_ADDRESS,
+        port=settings.REDIS_PORT,
+        health_check_interval=settings.REDIS_health_check_interval,
+    )
 
-@router.post('/production')
-def produce_model(n_trial: int, n_split:int, scoring : str, name : str, db:get_db=Depends()):
-    result= TrainService().train(n_trial=n_trial, n_split=n_split, scoring=scoring, name= name )
 
-    return  handle_result(result)
+@router.post("/production")
+def produce_model(
+    n_trial: int, n_split: int, scoring: str, name: str, db: get_db = Depends()
+):
+    result = TrainService().train(
+        n_trial=n_trial, n_split=n_split, scoring=scoring, name=name
+    )
+    return handle_result(result)
 
-@router.post('/income/predict',response_model=Item)
-def predict_income(item: IncomeBody, model_key : str, name : str):
-    result= PredictService(client, model_key, name).predict(item)
+
+@router.post("/income/predict", response_model=Item)
+def predict_income(item: IncomeBody, model_key: str, name: str):
+    result = PredictService(client, model_key, name).predict(item)
     return handle_result(result)
