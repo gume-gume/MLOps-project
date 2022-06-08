eaa9c6c6f874a19a01376a548597e3fc4f883456
jang-1996 <altu1996@naver.com>
2022-06-08 05:00:41 +0000

    <feat> check data
    크롤링해서 온 코인데이터의 null 값 및 4시간 간격으로 잘 받아와지나를 확인하는 코드입니다.

--- coin/data_verification.py
+++ coin/data_verification.py
@@ -1,11 +1,13 @@
-from coin_service import Coin_service
+from coin_service import cursor
 from config import settings
+import numpy as np
+import pandas as pd
 
-
-cursor = Coin_service.cursor
+cursor = cursor
 cursor.execute(f"""SELECT * FROM public."{settings.ticker}";""")
 data = cursor.fetchall()
 datetime_list = [time[0] for time in data]
+print("=====" * 10)
 
 
 def verify():
@@ -13,18 +15,31 @@ def verify():
 
 
 # 1  counting
-outlier = 10
-(len(datetime_list) / 6 - outlier) <= (
-    datetime_list[-1].date() - datetime_list[0].date()
-).days <= (len(datetime_list) / 6 + outlier)
+def coin_date_check(datetime_list):
+    datetime = []
+    for i in range(len(datetime_list)):
+        datetime.append(datetime_list[i].date())
+    unique_date_list = np.unique(datetime)
+    date_idx = pd.date_range(datetime_list[0].date(), datetime_list[-1].date())
+
+    no_data_idx = []
+    if len(date_idx) != len(unique_date_list):
+        print("결측 date 존재")
+        for i in range(len(date_idx)):
+            if date_idx[i].date() in unique_date_list:
+                pass
+            else:
+                no_data_idx.append(i)
+    return no_data_idx
 
 
 # 2 timedelta
-a = datetime_list[0]
-print(len(datetime_list))
-for i in datetime_list[1:]:
-    if (i - a).seconds / 60 / 60 == 4:
-        pass
-    else:
-        print("차이시간 : ", (i - a).seconds / 60 / 60)
-    a = i
+def coin_time_check(datetime_list):
+    datetime_list = np.unique(datetime_list)
+    for i in range(len(datetime_list) - 1):
+        if (datetime_list[i + 1] - datetime_list[i]).seconds / 60 / 60 != 4:
+            print(i, (datetime_list[i + 1] - datetime_list[i]).seconds / 60 / 60)
+
+
+coin_date_check(datetime_list)
+coin_time_check(datetime_list)
