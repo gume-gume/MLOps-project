eae98bd592dede800606e729e387dcfe252d8f3d
jang-1996 <altu1996@naver.com>
2022-06-19 18:36:04 +0000

    <refactoring> data verificarion
    데이터 검증입니다. 연산을 고려하였습니다.

--- coin/data_verification.py
+++ coin/data_verification.py
@@ -1,16 +1,15 @@
-from coin.coin_service import conn, cursor, Coin_service
-from coin.config import settings
-import numpy as np
+from coin_service import conn, cursor, engine, Coin_service
+from config import settings
 import pandas as pd
 import re
 import pyupbit
+import datetime
 
-cursor = cursor
+coin_service = Coin_service()
+
 cursor.execute(f"""SELECT * FROM public."{settings.ticker}";""")
 data = cursor.fetchall()
 datetime_list = [time[0] for time in data]
-
-coin_service = Coin_service()
 
 
 def cleanText(readData):
@@ -19,57 +18,68 @@ def cleanText(readData):
 
 
 def fill_data(null_date):
-    df = pd.DataFrame()
+    list_of_df = []
     for date in null_date:
-        df1 = pyupbit.get_ohlcv(
-            ticker="KRW-BTC", interval="minutes240", count=6, to=f"{date}", period=0.1
+        df_temp = pyupbit.get_ohlcv(
+            ticker=settings.ticker,
+            interval="minutes240",
+            count=6,
+            to=f"{date}",
+            period=0.1,
         )
-        df = pd.concat([df, df1])
-    return df
+        list_of_df.append(df_temp)
+    df_accum = pd.concat(list_of_df)
+    return df_accum
 
 
-def coin_date_check(datetime_list):
-    datetime = []
-    for i in range(len(datetime_list)):
-        datetime.append(datetime_list[i].date())
-    unique_date_list = np.unique(datetime)
-    date_idx = pd.date_range(datetime_list[0].date(), datetime_list[-1].date())
+def db_manage_command(cursor, text):
+    cursor.execute(text)
+    conn.commit()
 
-    null_date = []
 
-    if len(date_idx.date) != len(unique_date_list):
+def coin_date_check(datetime_list, engine):
+
+    db_date = pd.Series(datetime_list)
+    db_date = db_date.apply(lambda x: x.date()).drop_duplicates()
+    db_date.reset_index(drop=True, inplace=True)
+
+    continue_date = pd.date_range(
+        datetime_list[0].date(), datetime_list[-1].date()
+    ).to_series()
+    continue_date.reset_index(drop=True, inplace=True)
+    continue_date = continue_date.apply(lambda x: x.date())
+
+    if len(db_date) != len(continue_date):
         print("결측 date 존재")
-        for i in range(len(date_idx)):
-            if date_idx[i].date() in unique_date_list:
-                pass
-            else:
-                date = date_idx[i + 1].date().strftime("%Y-%m-%d")
-                null_date.append(cleanText(date))
-        coin_service.insert_df(fill_data(null_date), settings.ticker, exists="append")
-        null_date = []
-    else:
-        pass
+        check_df = pd.concat([db_date, continue_date]).reset_index(drop=True)
+        check_df.drop_duplicates(keep=False, inplace=True)
+        check_df.reset_index(drop=True, inplace=True)
+        check_df = check_df.apply(lambda x: x + datetime.timedelta(days=1))
+        check_df = check_df.apply(lambda x: cleanText(x.strftime("%Y-%m-%d")))
+        coin_service.insert_df(fill_data(check_df), settings.ticker, exists="append")
 
+    postgres_df = pd.read_sql("""SELECT * FROM public."{settings.ticker}";""", engine)
+    postgres_df.set_index("index", inplace=True)
+    postgres_df.drop_duplicates(inplace=True)
+    postgres_df = postgres_df.sort_index()
+    datetime_list = postgres_df.index
+
+    null_time = []
     for i in range(len(datetime_list) - 1):
         if (datetime_list[i + 1] - datetime_list[i]).seconds / 60 / 60 != 4:
             date = datetime_list[i + 1].date().strftime("%Y-%m-%d")
-            null_date.append(date)
+            null_time.append(date)
             coin_service.insert_df(
-                fill_data(null_date), settings.ticker, exists="append"
+                fill_data(null_time), settings.ticker, exists="append"
             )
-        else:
-            pass
 
-    cursor.execute(f"""SELECT * FROM public."{settings.ticker}" order by index;""")
-    conn.commit()
-    cursor.execute(
-        f"""DELETE FROM public."{settings.ticker}" WHERE ctid IN
-            (SELECT A.ctid FROM (SELECT ctid, ROW_NUMBER() over
-            (PARTITION BY index ORDER BY index)
-            AS num FROM public."{settings.ticker}")
-            A WHERE A.num > 1);"""
-    )
-    conn.commit()
+    postgres_df = pd.read_sql(f"""SELECT * FROM public."{settings.ticker}";""", engine)
+    postgres_df.set_index("index", inplace=True)
+    postgres_df.drop_duplicates(inplace=True)
+
+    coin_service.insert_df(postgres_df, settings.ticker, exists="append")
+
+    return postgres_df
 
 
-coin_date_check(datetime_list)
+coin_date_check(datetime_list, engine)
