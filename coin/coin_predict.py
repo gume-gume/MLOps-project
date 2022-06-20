74e98cfe07de848f6e541b7b0a2d70b43c995091
sunkyu <tjsrb63@gmail.com>
2022-06-20 06:28:36 +0000

    <feat> add mlflow coin scaler, scaler_y
    mlflow에 coin scaler를 불러올 수 있는 함수를 추가하였습니다.
    모델을 저장하는 방법과 같은 방식을 이용하여 작성하였습니다.
    
    >

--- coin/coin_predict.py
+++ coin/coin_predict.py
@@ -1,12 +1,12 @@
+import mlflow
+
 from coin.coin_service import Coin_service
 
 
 def train(ticker):
-    import mlflow
-
     cs = Coin_service()
     df = cs.data_load(ticker)
-    df, scaler_y = cs.scaling(df)
+    df, scaler, scaler_y = cs.scaling(df)
     x_train, x_test, y_train, y_test = cs.split(df)
     train_data = cs.windowed_dataset(y_train, True)
     test_data = cs.windowed_dataset(y_test, False)
@@ -14,9 +14,25 @@ def train(ticker):
     metrics = cs.confirm_result(y_test, y_pred)
     print(metrics)
 
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_tracking_uri("http://172.26.0.9:5000")
     mlflow.set_experiment(f"{ticker}_experiment")
     mlflow.start_run(tags={"version": "1.0.0"})
     mlflow.log_metrics(metrics)
     mlflow.keras.log_model(model, ticker, registered_model_name=ticker)
+    mlflow.keras.log_model(scaler, "scaler", registered_model_name="scaler")
+    mlflow.keras.log_model(scaler_y, "scaler_y", registered_model_name="scaler_y")
+
     mlflow.end_run()
+
+
+def predict_coin(ticker, coin_df):
+    mlflow.set_tracking_uri("http://172.26.0.9:5000")
+    mlflow.set_experiment(f"{ticker}_experiment")
+    model = mlflow.keras.load_model(f"models:/{ticker}/Production")
+    scaler = mlflow.keras.load_model("models:/scaler/Prodcution")
+    scaler_y = mlflow.keras.load_model("models:/scaler_y/Prodcution")
+
+    coin_df = scaler.transform(coin_df)
+
+    result = model.fit(coin_df)
+    return scaler_y.inverse_transform(result)
